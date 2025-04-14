1ef70ab1e9dd0adfdc16b09e0c62c517cc14c2d9
rashid <rashidrizan8@gamil.com>
2025-04-14 22:14:38 +0530

    all files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Container, createTheme, ThemeProvider } from "@mui/material";
+import Header from "./components/Header";
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <Container maxWidth="lg">
+        <Header />
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,47 @@
+import { Badge, Divider, IconButton, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import React from "react";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+  },
+  tagline: {
+    fontSize: 20, // fixed: use fontSize instead of "font"
+    flexGrow: 1,
+    justifyContent: "center",
+  },
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <>
+      <Toolbar>
+        <IconButton color="inherit">
+          <MenuIcon />
+        </IconButton>
+        <Typography className={classes.tagline} variant="h6">
+          Blogging website
+        </Typography>
+        <IconButton color="inherit">
+          <Badge badgeContent={4} color="secondary">
+            <NotificationsNoneIcon />
+          </Badge>
+        </IconButton>
+        <IconButton color="inherit">
+          <AccountCircleIcon />
+        </IconButton>
+      </Toolbar>
+      <Divider />
+      <Toolbar className={classes.tagline}>
+        Express your emotions through Images
+      </Toolbar>
+    </>
+  );
+};
+
+export default Header;
